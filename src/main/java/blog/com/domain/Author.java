ff2e0d3b51428d93f415212c28e22b8994b27664
Alex <aaadolgov@gmail.com>
2019-10-21 17:03:42 +0300

    1st commit

---
+++ src/main/java/blog/com/domain/Author.java
@@ -0,0 +1,33 @@
+package blog.com.domain;
+
+import lombok.Data;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+import java.io.Serializable;
+
+import static javax.persistence.GenerationType.AUTO;
+
+@Data
+@ToString(exclude = {"id", "password"}, includeFieldNames = true)
+@Entity
+@Table(name = "authors")
+public class Author implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = AUTO)
+    private Long id;
+
+    @Column(name = "password")
+    private String password;
+    @Column(name = "username", unique = true)
+    private String username;
+    @Column(name = "fullname")
+    private String fullname;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "update_by_email")
+    private boolean updateByEmail;
+
+}
---
+++ src/main/java/blog/com/domain/Message.java
@@ -0,0 +1,40 @@
+package blog.com.domain;
+
+import lombok.Data;
+import lombok.ToString;
+
+import javax.persistence.*;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+import static javax.persistence.GenerationType.AUTO;
+
+@Data
+@ToString(exclude = {"id"}, includeFieldNames = true)
+@Entity
+@Table(name = "messages")
+
+public class Message implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = AUTO)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    private Author author;
+
+    @Column(name = "spittleText")
+    private String text;
+
+    @Column(name = "postedTime")
+    private LocalDate when;
+
+    public Message() {
+        this.author = new Author();
+        this.author.setId((long) 1);
+    }
+}
